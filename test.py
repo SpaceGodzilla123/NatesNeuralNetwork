d2a753966d7e330a5dedce44162457c185fc5262
SpaceGodzilla123 <63753564+SpaceGodzilla123@users.noreply.github.com>
2020-05-27 09:09:58 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-
 # ----------------------
 # - read the input data:
 
@@ -8,21 +7,20 @@ training_data = list(training_data)
 
 # ---------------------
 # - network.py example:
-
+"""
 import clasNet.py
 
 
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
-
+"""
 
 # ----------------------
 # - network2.py example:
-"""import network2
+import natenet
 
 
-net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net = natenet.Network([784, 30, 10], cost=natenet.CrossEntropyCost)
 
 net.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,evaluation_data=validation_data,
     monitor_evaluation_accuracy=True)
-"""
